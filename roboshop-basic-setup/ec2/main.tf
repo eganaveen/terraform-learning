161ac240825278e16824a0adb4164712ce31f37e
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-25 19:44:44 +0530

    basic roboshop setup terraform

--- roboshop-basic-setup/ec2/main.tf
+++ roboshop-basic-setup/ec2/main.tf
@@ -1,18 +1,17 @@
 resource "aws_spot_instance_request" "spot_instance" {
-  ami           = "ami-0bb6af715826253bf"
-  instance_type = "t3.micro"
+  ami                    = "ami-0bb6af715826253bf"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
     Name = var.COMPONENT
   }
-
   provisioner "remote-exec" {
     connection {
-      type = "ssh"
-      user = "centos"
+      type     = "ssh"
+      user     = "centos"
       password = "DevOps321"
-      host = self.public_ip
+      host     = self.public_ip
     }
     inline = [
       "ansible-pull -U https://github.com/eganaveen/Ansible.git roboshop.yml -e HOSt=localhost -e role_name=${var.COMPONENT}"
