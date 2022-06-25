10835ea8d2b691d30d8e43c183536fa58e913b69
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-25 18:41:23 +0530

    remote provisioners

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_instance" {
   ami                    = "ami-0bb6af715826253bf"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg]
 
 #  provisioner "local-exec" {
