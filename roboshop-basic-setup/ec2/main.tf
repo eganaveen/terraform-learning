6b6381a2b1c261066e0edf7fc3380341da7843c4
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-25 20:11:44 +0530

    basic roboshop setup terraform

--- roboshop-basic-setup/ec2/main.tf
+++ roboshop-basic-setup/ec2/main.tf
@@ -2,6 +2,7 @@ resource "aws_spot_instance_request" "spot_instance" {
   ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
+  wait_for_fulfillment = true
 
   tags = {
     Name = var.COMPONENT
