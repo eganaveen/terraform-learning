9542f7baa44eda19a9ed9e6a2f7fe4a38085660c
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-25 20:17:26 +0530

    basic roboshop setup terraform

--- roboshop-basic-setup/ec2/main.tf
+++ roboshop-basic-setup/ec2/main.tf
@@ -15,7 +15,7 @@ resource "aws_spot_instance_request" "spot_instance" {
       host     = self.public_ip
     }
     inline = [
-      "ansible-pull -U https://github.com/eganaveen/Ansible.git roboshop.yml -e HOSt=localhost -e role_name=${var.COMPONENT}"
+      "ansible-pull -U https://github.com/eganaveen/Ansible.git roboshop.yml -e HOST=localhost -e role_name=${var.COMPONENT}"
     ]
   }
 }
